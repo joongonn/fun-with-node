078ee1efd178c139342bcedabe4a45dfaa055a11
joongonn <joongonn@gmail.com>
2016-03-13 22:59:54 -0700

    add winston logging

---
+++ logger.js
@@ -0,0 +1,26 @@
+var winston = require('winston');
+
+function formatter(args) {
+    var timestamp = new Date().toISOString()
+    var message = args.message;
+    if (!message && args.meta) {
+        message = `Exception stacktrace: ${args.meta.stack}`;
+    }
+
+    return `${timestamp} [${args.level.toUpperCase()}] ${message}`;
+}
+
+var logger = new winston.Logger({
+    transports: [
+        //TODO: rolling file appender
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            formatter: formatter
+        })
+    ],
+    exitOnError: false
+});
+
+module.exports = logger;
